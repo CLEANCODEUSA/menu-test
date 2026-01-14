910a3ec478508583316820d22f7b7b1fbe12185d
CLEANCODE <help@simplecssjs.com>
2026-01-13 20:36:34 -0500

    Create StackNavigator.js

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Home from '../screens/Home'
+import About from '../screens/About'
+
+const Stack = createStackNavigator()
+
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home} />
+      <Stack.Screen name='About' component={About} />
+    </Stack.Navigator>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export { MainStackNavigator }
