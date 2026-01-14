33347e58a011bf3399ac2816e85008a61123a9bb
CLEANCODE <help@simplecssjs.com>
2026-01-13 20:30:59 -0500

    Create Contact.js

---
+++ screens/Contact.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <View style={styles.center}>
+      <Text>Contact</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+})
+
+export default Contact
