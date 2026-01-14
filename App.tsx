ebebb0ed4fbcf97da87799732235b378e60f6d56
CLEANCODE <help@simplecssjs.com>
2026-01-13 20:20:09 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigatoin/native'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -9,6 +11,8 @@ export default function App() {
     </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
