64ce54870075e0cd22cb22f9564954fee350e0ac
CLEANCODE <help@simplecssjs.com>
2026-01-13 21:04:18 -0500

    Update StackNavigator.js

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,20 +3,21 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from '../screens/Home'
 import About from '../screens/About'
+import Contact from '../screens/Contact'
 
 const Stack = createStackNavigator()
 
+const screenOptionsStyle = {
+  headerStyle: {
+    backgroundColor: '#9ac4f8'
+  },
+  headerTintColor: 'white',
+  headerBackTitle: 'Back'
+}
+
 const MainStackNavigator = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#9ac4f8'
-        },
-        headerTintColor: 'white',
-        headerBackTitle: 'Back'
-      }}
-    >
+    <Stack.Navigator screenOptions={screenOptionsStyle}>
       <Stack.Screen name='Home' component={Home} />
       <Stack.Screen name='About' component={About} />
     </Stack.Navigator>
