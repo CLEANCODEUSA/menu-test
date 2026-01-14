8da854364c02570113581d7c63a06e073a1eb915
CLEANCODE <help@simplecssjs.com>
2026-01-13 20:40:59 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react'
 import { NavigationContainer } from '@react-navigatoin/native'
@@ -7,7 +6,6 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
