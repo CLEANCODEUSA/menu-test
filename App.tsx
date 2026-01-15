15cacea1a1cba9560eefa83a010f59f10eaa9258
CLEANCODE <help@simplecssjs.com>
2026-01-14 19:09:00 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 
-import { MainStackNavigator } from './navigation/StackNavigator'
+import { BottomTabNavigator } from './navigation/TabNavigator'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <MainStackNavigator />
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
